aff86eec5af0cb8fcf07dcd9f08ba771bdfa0c10
krisciu <kriscija@gmail.com>
2023-10-26 22:11:23 -0700

    fix more colors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,8 +17,8 @@ const Hero: React.FC<HeroProps> = ({ title, subtitle, image }) => {
     return (
         <div className="flex flex-row items-center justify-center h-3/4 bg-slate-600" style={bgImageStyle}>
             <div className="space-y-4">
-                <h1 className="text-5xl font-bold text-navy-blue drop-shadow-xl text-center">{title}</h1>
-                <h2 className="text-3xl text-gray-300 drop-shadow-sm text-center">{subtitle}</h2>
+                <h1 className="text-5xl font-bold text-red-400 drop-shadow-xl text-center">{title}</h1>
+                <h2 className="text-3xl text-pink-300 drop-shadow-sm text-center">{subtitle}</h2>
                 <SignupForm />
             </div>
         </div>
